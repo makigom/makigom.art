b5064fbd5975f735182be047b348edee54a2e96d
Macarena Gomez <macarena@p22.studio>
2024-04-15 14:59:12 +0200

    feat: hero wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌼</text></svg>"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>"
       ></link>
       <body className={courier.className}>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="h-screen">
       <Hero />
+      <h1 className="absolute bottom-10 left-16 font-bold text-6xl">makigom</h1>
     </main>
   );
 }
